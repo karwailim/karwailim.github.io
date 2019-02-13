59bbe13bc6e5f15584c22757529190695847bf44
Kar Wai Lim <karwai.lim@gmail.com>
2019-02-13 21:51:36 +0800

    Update site

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 mv Home.html index.html
 find . -name '.DS_Store' -type f -delete
-sed -i '' 's/&amp;sa=D[^"]*//g; s/%3A/:/g; s/%2F/\//g; s/%3F/?/g; s/%3D/=/g; s/http[s]*:\/\/www.google.com\/url?q=//g; s/http[s]*:\/\/karwailim.github.io\//.\//g' index.html
+sed -i '' 's/&amp;sa=D[^"]*//g; s/%3A/:/g; s/%2F/\//g; s/%3F/?/g; s/%3D/=/g; s/http[s]*:\/\/www.google.com\/url?q=//g; s/http[s]*:\/\/karwailim.github.io\//.\//g; s/<meta name="robots" content="noindex"\/>//g' index.html
